e46bfbc3187dc632e5b848d2ed1b65922bf7f59a
polankivarunkumar <34741081+polankivarunkumar@users.noreply.github.com>
2023-09-05 22:31:26 +0530

    Add files via upload

---
+++ IOS-Discussions/IOS-Discussions/IOS_DiscussionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_DiscussionsApp.swift
+//  IOS-Discussions
+//
+//  Created by Varun on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_DiscussionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
